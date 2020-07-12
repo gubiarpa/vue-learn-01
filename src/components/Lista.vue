577267408b115eee159334d1bc7509ffe48a289e
gubiarpa <billy.arredondo@pucp.pe>
2020-07-12 02:21:19 -0500

    Using Computed Property
    Se usó una propiedad computada para que se renderice actualizadamente una propiedad. del state. También se usó la función 'sort' de Javascript.

--- src/components/Lista.vue
+++ src/components/Lista.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <ul class="list-group">
-        <li v-for="(fruta, index) in frutas" :key="fruta.id" @click="aumentar(index)" class="list-group-item d-flex justify-content-between align-items-center">
+        <li v-for="(fruta, index) in frutasOrdenado" :key="fruta.id" @click="aumentar(index)" class="list-group-item d-flex justify-content-between align-items-center">
           {{ index }} - {{ fruta.nombre }}
           <span class="badge badge-primary badge-pill">{{ fruta.cantidad }}</span>
         </li>
@@ -16,7 +16,11 @@ import { mapState, mapMutations } from 'vuex'
 export default {
     name: 'Cabecera',
     computed: {
-      ...mapState(['frutas'])
+      ...mapState(['frutas']),
+      frutasOrdenado(){
+        return this.frutas.sort((a, b) => b.cantidad - a.cantidad);
+          // esto ordena de mayor a menor
+      }
     },
     methods: {
       ...mapMutations(['aumentar', 'limpiar'])
