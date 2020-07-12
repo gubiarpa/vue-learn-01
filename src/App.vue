100e94f09525ca5bcb30296ffa77a83a353eb202
gubiarpa <billy.arredondo@pucp.pe>
2020-07-12 01:33:59 -0500

    Initial State
    Solo contiene componentes para un proyecto simple.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Cabecera></Cabecera>
+    <Lista></Lista>
+  </div>
+</template>
+
+<script>
+import Cabecera from './components/Cabecera'
+import Lista from './components/Lista'
+
+export default {
+  name: 'App',
+  components: {
+    Cabecera,
+    Lista
+  }
+}
+</script>
